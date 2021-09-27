795d10c5607a9b5abc7325d566fca0be3dc33d2a
stephanegaiffas <stephane.gaiffas@gmail.com>
2021-09-27 16:36:01 +0200

    Adds an efficient sum_sq function specialized to F and C-major ndarrays and sparse CSC and CSR matrices

--- linlearn/_utils.py
+++ linlearn/_utils.py
@@ -1,8 +1,17 @@
-from numba import jit, void, uintp, float64
+"""
+This module contains some utilities used throughout linlearn.
+"""
+
+# Authors: Stephane Gaiffas <stephane.gaiffas@gmail.com>
+#          Ibrahim Merad <imerad7@gmail.com>
+# License: BSD 3 clause
+
+
 import numpy as np
 from numpy.random import randint
+from scipy.sparse import issparse, isspmatrix_csr, isspmatrix_csc
+from numba import jit, void, uintp, prange, float64
 
-import numba as nb
 
 # Numba flags applied to all jit decorators
 NOPYTHON = True
@@ -10,9 +19,423 @@ NOGIL = True
 BOUNDSCHECK = False
 FASTMATH = True
 
+jit_kwargs = {
+    "nopython": NOPYTHON,
+    "nogil": NOGIL,
+    "boundscheck": BOUNDSCHECK,
+    "fastmath": FASTMATH,
+}
 
-nb_float = nb.float64
+nb_float = float64
 np_float = np.float64
+
+
+@jit(
+    nopython=NOPYTHON,
+    nogil=NOGIL,
+    boundscheck=BOUNDSCHECK,
+    fastmath=FASTMATH
+    # locals={"size": uintp, "left": intp, "right": intp, "middle": intp},
+)
+def is_in_sorted(i, v):
+    """Tests if i value i is in v.
+
+    Parameters
+    ----------
+    i : int
+        The index
+
+    v : ndarray
+        array of shape (size,)
+
+    Returns
+    -------
+    output : bool
+        True if i is in v, False otherwise
+    """
+    size = v.size
+    if size == 0:
+        return False
+    elif size == 1:
+        return v[0] == i
+    else:
+        # We perform a binary search
+        left, right = 0, size - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if v[middle] == i:
+                return True
+            else:
+                if v[middle] < i:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+        return False
+
+
+@jit(nopython=NOPYTHON, nogil=NOGIL, boundscheck=BOUNDSCHECK, fastmath=FASTMATH)
+def whereis_sorted(i, v):
+    size = v.size
+    if size == 0:
+        return -1
+    elif size == 1:
+        if v[0] == i:
+            # i is the only element in v at index 0
+            return 0
+        else:
+            # i is not in v
+            return -1
+    else:
+        # We perform a binary search
+        left, right = 0, size - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if v[middle] == i:
+                return middle
+            else:
+                if v[middle] < i:
+                    left = middle + 1
+                else:
+                    right = middle - 1
+        return -1
+
+
+@jit(nopython=NOPYTHON, nogil=NOGIL, boundscheck=BOUNDSCHECK, fastmath=FASTMATH)
+def csr_get(indptr, indices, data, i, j):
+    """
+
+    Parameters
+    ----------
+    data
+    indices
+    indptr
+    i
+    j
+
+    Returns
+    -------
+
+    """
+    row_start = indptr[i]
+    row_end = indptr[i + 1]
+    idx = whereis_sorted(j, indices[row_start:row_end])
+    if idx >= 0:
+        return data[row_start + idx]
+    else:
+        return 0.0
+
+
+def matrix_type(X):
+    """Returns the matrix type of the input matrix in the form of a string, indicating
+    if it is dense F-major, dense C-major, sparse CSC or sparse CSR. Other types will
+    raise an error.
+
+    This function must be used internally only.
+
+    Parameters
+    ----------
+    X : {array-like, sparse matrix} of shape (n_samples, n_features)
+        Matrix of training vectors, where n_samples is the number of samples and
+        n_features is the number of features.
+
+    Returns
+    -------
+    output : {'csc', 'csr', 'f', 'c'}
+        The output is
+        - 'csc' if X is sparse CSC
+        - 'csr' if X is sparse CSR
+        - 'c' if it is dense (a 2D numpy array) and C-major
+        - 'f' if it is dense (a 2D numpy array) and F-major
+    """
+    if issparse(X):
+        if isspmatrix_csc(X):
+            return "csc"
+        elif isspmatrix_csr(X):
+            return "csr"
+        else:
+            raise ValueError("Only sparse CSC and CSR matrices are supported.")
+    else:
+        if X.flags.c_contiguous:
+            return "c"
+        elif X.flags.f_contiguous:
+            return "f"
+        else:
+            raise ValueError("Only C and F-major numpy arrays are supported.")
+
+
+@jit(**jit_kwargs, parallel=True)
+def col_sq_sum_f(X, out):
+    """Computes the sums of squares of the columns of the F-major matrix X.
+
+    Parameters
+    ----------
+    X : numpy.ndarray of shape (n_samples, n_features)
+        Input F-major matrix
+
+    out : numpy.array of shape (n_features,)
+        Array containing the sums of squares of the columns of X
+    """
+    n_samples, n_features = X.shape
+    for j in prange(n_features):
+        col_j_squared_norm = 0.0
+        for i in range(n_samples):
+            col_j_squared_norm += X[i, j] ** 2
+        out[j] = col_j_squared_norm
+
+
+@jit(**jit_kwargs, parallel=True)
+def col_sq_sum_c(X, out):
+    """Computes the sums of squares of the columns of the C-major matrix X.
+
+    Parameters
+    ----------
+    X : numpy.ndarray of shape (n_samples, n_features)
+        Input C-major matrix
+
+    out : numpy.array of shape (n_features,)
+        Array containing the sums of squares of the columns of X
+    """
+    n_samples, n_features = X.shape
+    out.fill(0.0)
+    for i in prange(n_samples):
+        for j in range(n_features):
+            out[j] += X[i, j] ** 2
+
+    return out
+
+
+@jit(**jit_kwargs, parallel=True)
+def col_sq_sum_csc(n_samples, n_features, X_indptr, X_indices, X_data, out):
+    """Computes the sums of squares of the columns of a sparse CSC matrix X.
+
+    Parameters
+    ----------
+    n_samples : int
+        Number of rows in the input matrix
+
+    n_features : int
+        Number of columns in the input matrix
+
+    X_indptr : ndarray
+        Array containing X.indptr
+
+    X_indices : ndarray
+        Array containing X.indices
+
+    X_data : ndarray
+        Array containing X.data
+
+    out : numpy.array of shape (n_features,)
+        Array containing the sums of squares of the columns of X
+    """
+    for j in prange(n_features):
+        col_j_squared_norm = 0.0
+        col_start = X_indptr[j]
+        col_end = X_indptr[j + 1]
+        for idx in range(col_start, col_end):
+            col_j_squared_norm += X_data[idx] ** 2
+        out[j] = col_j_squared_norm
+
+
+@jit(**jit_kwargs, parallel=True)
+def col_sq_sum_csr(n_samples, n_features, X_indptr, X_indices, X_data, out):
+    """Computes the sums of squares of the columns of a sparse CSR matrix X.
+
+    Parameters
+    ----------
+    n_samples : int
+        Number of rows in the input matrix
+
+    n_features : int
+        Number of columns in the input matrix
+
+    X_indptr : ndarray
+        Array containing X.indptr
+
+    X_indices : ndarray
+        Array containing X.indices
+
+    X_data : ndarray
+        Array containing X.data
+
+    out : numpy.array of shape (n_features,)
+        Array containing the sums of squares of the columns of X
+    """
+    out.fill(0.0)
+    for i in prange(n_samples):
+        row_start = X_indptr[i]
+        row_end = X_indptr[i + 1]
+        for idx in range(row_start, row_end):
+            j = X_indices[idx]
+            out[j] += X_data[idx] ** 2
+
+
+@jit(**jit_kwargs, parallel=True)
+def row_sq_sum_f(X, out):
+    """Computes the sums of squares of the rows of the F-major matrix X.
+
+    Parameters
+    ----------
+    X : numpy.ndarray of shape (n_samples, n_features)
+        Input F-major matrix
+
+    out : numpy.array of shape (n_samples,)
+        Array containing the sums of squares of the rows of X
+    """
+    n_samples, n_features = X.shape
+    out.fill(0.0)
+    for j in prange(n_features):
+        for i in range(n_samples):
+            out[i] += X[i, j] ** 2
+
+
+@jit(**jit_kwargs, parallel=True)
+def row_sq_sum_c(X, out):
+    """Computes the sums of squares of the rows of the C-major matrix X.
+
+    Parameters
+    ----------
+    X : numpy.ndarray of shape (n_samples, n_features)
+        Input C-major matrix
+
+    out : numpy.array of shape (n_samples,)
+        Array containing the sums of squares of the rows of X
+    """
+    n_samples, n_features = X.shape
+    for i in prange(n_samples):
+        row_i_squared_norm = 0.0
+        for j in range(n_features):
+            row_i_squared_norm += X[i, j] ** 2
+        out[i] = row_i_squared_norm
+
+    return out
+
+
+@jit(**jit_kwargs, parallel=True)
+def row_sq_sum_csc(n_samples, n_features, X_indptr, X_indices, X_data, out):
+    """Computes the sums of squares of the rows of a sparse CSC matrix X.
+
+    Parameters
+    ----------
+    n_samples : int
+        Number of rows in the input matrix
+
+    n_features : int
+        Number of columns in the input matrix
+
+    X_indptr : ndarray
+        Array containing X.indptr
+
+    X_indices : ndarray
+        Array containing X.indices
+
+    X_data : ndarray
+        Array containing X.data
+
+    out : numpy.array of shape (n_samples,)
+        Array containing the sums of squares of the rows of X
+    """
+    out.fill(0.0)
+    for j in prange(n_features):
+        col_start = X_indptr[j]
+        col_end = X_indptr[j + 1]
+        for idx in range(col_start, col_end):
+            i = X_indices[idx]
+            out[i] += X_data[idx] ** 2
+
+
+@jit(**jit_kwargs, parallel=True)
+def row_sq_sum_csr(n_samples, n_features, X_indptr, X_indices, X_data, out):
+    """Computes the sums of squares of the rows of a sparse CSR matrix X.
+
+    Parameters
+    ----------
+    n_samples : int
+        Number of rows in the input matrix
+
+    n_features : int
+        Number of columns in the input matrix
+
+    X_indptr : ndarray
+        Array containing X.indptr
+
+    X_indices : ndarray
+        Array containing X.indices
+
+    X_data : ndarray
+        Array containing X.data
+
+    out : numpy.array of shape (n_samples,)
+        Array containing the sums of squares of the rows of X
+    """
+    for i in prange(n_samples):
+        row_i_squared_norm = 0.0
+        row_start = X_indptr[i]
+        row_end = X_indptr[i + 1]
+        for idx in range(row_start, row_end):
+            row_i_squared_norm += X_data[idx] ** 2
+        out[i] = row_i_squared_norm
+
+
+def sum_sq(X, axis, out=None):
+    """Computes the sum of squares (squared Euclidean norm) along given axis. It
+    supports efficient implementations both for F and C-major numpy arrays and sparse
+    CSC and CSR matrices.
+
+    Parameters
+    ----------
+    X : {array-like, sparse matrix} of shape (n_samples, n_features)
+        Matrix of training vectors, where n_samples is the number of samples and
+        n_features is the number of features. This function supports F-major and
+        C-major ndarray, CSC and CSR sparse matrices.
+
+    axis : {0, 1}
+        Axis along which with compute the sum of squares.
+
+    out : ndarray or None
+        If ndarray, then the sum of squares are saved in the given vector and is
+        returned. Otherwise a new ndarray is allocated. Note that ``out`` has shape
+        (n_features,) if ``axis=0`` and (n_samples,) if ``axis=1 ``.
+
+    Returns
+    -------
+    output : ndarray
+        An array containing the sum of squares along axis. Note that ``out`` has shape
+        (n_features,) if ``axis=0`` and (n_samples,) if ``axis=1``. If ``out`` is
+        used, this is the same ndarray as the one passed.
+    """
+    n_samples, n_features = X.shape
+    mtype = matrix_type(X)
+    if axis == 0:
+        if out is None:
+            out = np.empty(n_features)
+        if mtype == "f":
+            col_sq_sum_f(X, out)
+        elif mtype == "c":
+            col_sq_sum_c(X, out)
+        elif mtype == "csc":
+            col_sq_sum_csc(n_samples, n_features, X.indptr, X.indices, X.data, out)
+        elif mtype == "csr":
+            col_sq_sum_csr(n_samples, n_features, X.indptr, X.indices, X.data, out)
+        else:
+            raise NotImplementedError()
+    elif axis == 1:
+        if out is None:
+            out = np.empty(n_samples)
+        if mtype == "f":
+            row_sq_sum_f(X, out)
+        elif mtype == "c":
+            row_sq_sum_c(X, out)
+        elif mtype == "csc":
+            row_sq_sum_csc(n_samples, n_features, X.indptr, X.indices, X.data, out)
+        elif mtype == "csr":
+            row_sq_sum_csr(n_samples, n_features, X.indptr, X.indices, X.data, out)
+        else:
+            raise NotImplementedError()
+    else:
+        raise ValueError("Only axis=0 and axis=1 are supported")
+
+    return out
 
 
 def get_type(class_):
---
+++ tests/test_utils.py
@@ -0,0 +1,121 @@
+"""
+This module contains unittests for some functions from _utils
+"""
+
+# Authors: Stephane Gaiffas <stephane.gaiffas@gmail.com>
+#          Ibrahim Merad <imerad7@gmail.com>
+# License: BSD 3 clause
+
+import numpy as np
+from scipy.sparse import csr_matrix, csc_matrix
+
+import pytest
+
+from linlearn._utils import is_in_sorted, whereis_sorted, csr_get, matrix_type, sum_sq
+
+
+@pytest.mark.parametrize(
+    "v", [np.array([]), np.array([3]), np.array([3, 4]), np.array([3, 4, 8]),],
+)
+@pytest.mark.parametrize(
+    "i", range(13),
+)
+def test_is_in_sorted(v, i):
+    in_partition = i in v
+    assert is_in_sorted(i, v) == in_partition
+
+
+@pytest.mark.parametrize(
+    "v",
+    [
+        np.array([]),
+        np.array([3]),
+        np.array([3, 4]),
+        np.array([3, 4, 8], dtype=np.uint8),
+    ],
+)
+@pytest.mark.parametrize(
+    "i", range(13),
+)
+def test_whereis_sorted(v, i):
+    def argwhere(i, v):
+        w = np.argwhere(v == i).ravel()
+        if w.size > 0:
+            return w[0]
+        else:
+            return -1
+
+    assert whereis_sorted(i, v) == argwhere(i, v)
+
+
+@pytest.mark.parametrize("sparsify", ("no", "random", "row", "col"))
+def test_csr_get(sparsify):
+    n_samples, n_features = 11, 4
+    rng = np.random.RandomState(42)
+    X_dense = rng.randn(n_samples, n_features)
+    if sparsify == "random":
+        X_dense[X_dense < 0.0] = 0.0
+    elif sparsify == "row":
+        X_dense[::2, :] = 0.0
+    elif sparsify == "col":
+        X_dense[:, ::2] = 0.0
+
+    X_csr = csr_matrix(X_dense)
+    rows = []
+    for i in range(n_samples):
+        row = []
+        for j in range(n_features):
+            w = csr_get(X_csr.indptr, X_csr.indices, X_csr.data, i, j)
+            row.append(w)
+        rows.append(row)
+
+    assert X_dense == pytest.approx(np.array(rows), abs=1e-15)
+
+
+@pytest.mark.parametrize("mtype", ("c", "f", "csr", "csc"))
+def test_matrix_type(mtype):
+    n_samples, n_features = 11, 4
+    rng = np.random.RandomState(42)
+    X = rng.randn(n_samples, n_features)
+    if mtype == "c":
+        X = np.ascontiguousarray(X)
+    elif mtype == "f":
+        X = np.asfortranarray(X)
+    elif mtype == "csc":
+        X = csc_matrix(X)
+    else:
+        X = csr_matrix(X)
+    assert mtype == matrix_type(X)
+
+
+@pytest.mark.parametrize("mtype", ("c", "f", "csr", "csc"))
+@pytest.mark.parametrize("sparsify", ("no", "random", "row", "col"))
+@pytest.mark.parametrize("axis", (0, 1))
+def test_sum_sq(mtype, sparsify, axis):
+    n_samples, n_features = 11, 4
+    rng = np.random.RandomState(42)
+    X = rng.randn(n_samples, n_features)
+    if sparsify == "random":
+        X[X < 0.0] = 0.0
+    elif sparsify == "row":
+        X[::2, :] = 0.0
+    elif sparsify == "col":
+        X[:, :2] = 0.0
+    norms = np.linalg.norm(X, 2, axis=axis) ** 2
+    if mtype == "c":
+        X = np.ascontiguousarray(X)
+    elif mtype == "f":
+        X = np.asfortranarray(X)
+    elif mtype == "csc":
+        X = csc_matrix(X)
+    else:
+        X = csr_matrix(X)
+
+    if axis == 0:
+        out = np.empty(n_features)
+    else:
+        out = np.empty(n_samples)
+
+    sum_sq(X, axis=axis, out=out)
+    assert norms == pytest.approx(out, abs=1e-12)
+    assert norms == pytest.approx(sum_sq(X, axis=axis), abs=1e-12)
