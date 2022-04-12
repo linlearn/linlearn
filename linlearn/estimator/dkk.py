bc16ea01dae481e2cf53802839ed98ec0a17a488
Merad <merad@MacBook-Pro-de-Merad.local>
2022-04-12 19:09:31 +0200

    added dkk

---
+++ linlearn/estimator/dkk.py
@@ -0,0 +1,184 @@
+# Authors: Stephane Gaiffas <stephane.gaiffas@gmail.com>
+#          Ibrahim Merad <imerad7@gmail.com>
+# License: BSD 3 clause
+
+
+from collections import namedtuple
+import numpy as np
+from numba import jit
+from ._base import Estimator, jit_kwargs
+from .._utils import np_float
+
+
+@jit(**jit_kwargs)
+def find_t(g, w, eps):
+    p1, p2 = int((len(g)-1)/max(1, 2*(1 - 2*eps))), (len(g)-1)
+    while p2 - p1 > 2:
+        mid = (p1 + p2) // 2
+        t = np.partition(g, mid)[mid]
+        sm = 0.0
+        for i in range(len(g)):
+            if g[i] >= t:
+                sm += w[i]
+        if sm >= eps:
+            p1 = mid
+        else:
+            p2 = mid
+    return t
+
+@jit(**jit_kwargs)
+def dkk(vecs, eps):
+    n, d = vecs.shape
+    w = np.empty(len(vecs))
+    ph1 = np.empty(vecs.shape)
+    Sigma = np.empty((d, d))
+    w.fill(1 / len(vecs))
+    sum_w = 1.0
+    while sum_w > 1 - 2 * eps:
+        mu = np.dot(w, vecs) / sum_w
+
+        for i in range(n):
+            for j in range(d):
+                ph1[i, j] = vecs[i, j] - mu[j]
+        # ph1 = vecs - mu[np.newaxis, :]
+
+        Sigma.fill(0.0)
+        for j1 in range(d):
+            for j2 in range(j1, d):
+                for i in range(n):
+                    Sigma[j1, j2] += w[i] * ph1[i, j1] * ph1[i, j2]
+        Sigma /= sum_w
+        Sigma += Sigma.T
+        for j in range(d):
+            Sigma[j, j] /= 2
+
+        #Sigma = ph1.T @ (w[:, np.newaxis] * ph1)
+        # TODO : Figure out how to compute only first eigenvector in Numba
+        eigvals, eigvecs = np.linalg.eigh(Sigma)
+        eigvec = eigvecs[:, np.argmax(eigvals)]
+        #_, eigvec = scipy.linalg.eigh(Sigma, subset_by_index=[d - 1, d - 1])
+
+        g = np.square(ph1 @ eigvec).reshape(n)
+
+        #________________________________
+        asg = np.argsort(g)#[::-1]
+        sm = 0.0
+        ind = n-1
+        while sm < eps:
+            sm += w[asg[ind]]
+            ind -= 1
+        t = g[asg[ind + 1]]
+        #________________________________
+        # t = find_t(g, w, eps)
+
+        f = g#.copy()
+        # f[f < t] = 0
+        m = 0.0
+        for i in range(n):
+            if f[i] < t:
+                f[i] = 0.0
+            elif f[i] > m and w[i] > 0:
+                m = f[i]
+        w = np.multiply(w, 1 - f / m)
+        sum_w = np.sum(w)
+
+    return np.dot(w, vecs) / sum_w
+
+
+
+
+
+StateDKK = namedtuple(
+    "StateDKK",
+    [
+        "deriv_samples",
+        "deriv_samples_outer_prods",
+        "gradient",
+        "loss_derivative",
+    ],
+)
+
+
+class DKK(Estimator):
+    def __init__(self, X, y, loss, n_classes, fit_intercept, eps):
+        super().__init__(X, y, loss, n_classes, fit_intercept)
+        self.eps = eps
+
+    def get_state(self):
+        return StateDKK(
+            deriv_samples=np.empty(
+                (self.n_samples, self.n_classes), dtype=np_float, order="F"
+            ),
+            deriv_samples_outer_prods=np.empty(
+                (self.n_samples, self.n_classes), dtype=np_float, order="F"
+            ),
+            gradient=np.empty(
+                (self.n_features + int(self.fit_intercept), self.n_classes),
+                dtype=np_float,
+            ),
+            loss_derivative=np.empty(self.n_classes, dtype=np_float),
+        )
+
+    def partial_deriv_factory(self):
+        raise ValueError(
+            "dkk estimator does not support CGD, use mom/tmean/ch estimator instead"
+        )
+
+    def grad_factory(self):
+        X = self.X
+        y = self.y
+        loss = self.loss
+        deriv_loss = loss.deriv_factory()
+        n_samples = self.n_samples
+        n_features = self.n_features
+        n_classes = self.n_classes
+        eps = self.eps
+
+        if self.fit_intercept:
+
+            @jit(**jit_kwargs)
+            def grad(inner_products, state):
+                deriv_samples = state.deriv_samples
+                deriv_samples_outer_prods = state.deriv_samples_outer_prods
+                gradient = state.gradient
+
+                for i in range(n_samples):
+                    deriv_loss(y[i], inner_products[i], deriv_samples[i])
+
+                    for k in range(n_classes):
+                        deriv_samples_outer_prods[i, k] = deriv_samples[i, k]
+
+                gradient[0, :] = dkk(deriv_samples_outer_prods, eps)
+
+                for j in range(n_features):
+                    for k in range(n_classes):
+                        for i in range(n_samples):
+                            deriv_samples_outer_prods[i, k] = (
+                                deriv_samples[i, k] * X[i, j]
+                            )
+                    gradient[j + 1, :] = dkk(deriv_samples_outer_prods, eps)
+
+            return grad
+        else:
+
+            @jit(**jit_kwargs)
+            def grad(inner_products, state):
+                deriv_samples = state.deriv_samples
+                deriv_samples_outer_prods = state.deriv_samples_outer_prods
+                gradient = state.gradient
+
+                for i in range(n_samples):
+                    deriv_loss(y[i], inner_products[i], deriv_samples[i])
+
+                for j in range(n_features):
+
+                    for k in range(n_classes):
+                        for i in range(n_samples):
+                            deriv_samples_outer_prods[i, k] = (
+                                    deriv_samples[i, k] * X[i, j]
+                            )
+
+                    gradient[j, :] = dkk(deriv_samples_outer_prods, eps)
+
+                return 0
+            return grad
